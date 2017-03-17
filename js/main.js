ce0f347bfcaa785fd59310540672385af4bd398b
Rubén Toca Lucio <rubentoca@gmail.com>
2017-03-17 02:52:04 +0100

    aplicación de estilos

--- js/main.js
+++ js/main.js
@@ -1,44 +1,42 @@
-var isMenuLateral = false,
-    isSeccionVisible = false;
+var isMenuLateral = false
+    , isSeccionVisible = false;
 
-function Ajax(url, elemento){
+function Ajax(url, elemento) {
     var xmlhttp;
-    if (window.XMLHttpRequest)
-      {// code for IE7+, Firefox, Chrome, Opera, Safari
-      xmlhttp=new XMLHttpRequest();
-      }
-    else
-      {// code for IE6, IE5
-      xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-      }
-    xmlhttp.open("GET",url,false);
+    if (window.XMLHttpRequest) { // code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp = new XMLHttpRequest();
+    }
+    else { // code for IE6, IE5
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.open("GET", url, false);
     xmlhttp.send();
     elemento.innerHTML = xmlhttp.responseText;
 }
 
-function menuLateral(){
-   if (isMenuLateral) return;
-   var menu = document.querySelector("nav");
-   menu.style.textAlign = "left";
-   menu.style.margin = "0%";
-   menu.style.height = 'auto';
-   menu.style.width = '20%';
-   menu.style.float = 'left';
-   var menu_a = document.querySelectorAll("nav a");
-   for (i = 0; i < menu_a.length; i++){
+function menuLateral() {
+    if (isMenuLateral) return;
+    var menu = document.querySelector("nav");
+    menu.style.textAlign = "left";
+    menu.style.margin = "0%";
+    menu.style.height = 'auto';
+    menu.style.width = '20%';
+    menu.style.float = 'left';
+    var menu_a = document.querySelectorAll("nav a");
+    for (i = 0; i < menu_a.length; i++) {
         menu_a[i].style.fontSize = "50%";
         menu_a[i].style.display = "inline";
         menu_a[i].style.marginTop = "5%";
-   }
-   var titulo = document.querySelector("header");
-   if (titulo != null){
+    }
+    var titulo = document.querySelector("header");
+    if (titulo != null) {
         padre = titulo.parentNode;
         padre.removeChild(titulo);
-   }
-   isMenuLateral = true;
+    }
+    isMenuLateral = true;
 };
 
-function seccionVisible(){
+function seccionVisible() {
     if (isSeccionVisible) return;
     var seccion = document.querySelector("section");
     seccion.style.display = "inline";
@@ -47,49 +45,49 @@ function seccionVisible(){
     isSeccionVisible = true;
 };
 
-function ocultarArticulos(){
-   var tao = document.getElementById("taoteking"),
-       hua = document.getElementById("huahuching"),
-       wen = document.getElementById("wentzu");
-   tao.style.display = hua.style.display = wen.style.display = "none";
-   tao.innerHTML = hua.innerHTML = wen.innerHTML = "";
+function ocultarArticulos() {
+    var tao = document.getElementById("taoteking")
+        , hua = document.getElementById("huahuching")
+        , wen = document.getElementById("wentzu");
+    tao.style.display = hua.style.display = wen.style.display = "none";
+    tao.innerHTML = hua.innerHTML = wen.innerHTML = "";
 };
 
-function actualizar(){
-  var  body = document.querySelector('body'),
-       contenido = document.getElementById('contenedor'),
-       width = window.innerWidth,
-       height = window.innerHeight;
-   // dimensiono body para que ocupe solo la ventana 
-   body.style.width = width.toString() + 'px';
-   body.style.height = height.toString() + 'px';
-   // dimensiono contenido al 85%w y 90%h de body
-   contenido.style.width = ((width*85)/100).toString() + 'px';
-   contenido.style.height = ((height*90)/100).toString() + 'px';
-   contenido.style.marginTop = '1%';
-   // posiciono el logo desde el final del contenido
-   document.getElementById('logo').style.bottom = '8%';
+function actualizar() {
+    var body = document.querySelector('body')
+        , contenido = document.getElementById('contenedor')
+        , width = window.innerWidth
+        , height = window.innerHeight;
+    // dimensiono body para que ocupe solo la ventana 
+    body.style.width = width.toString() + 'px';
+    body.style.height = height.toString() + 'px';
+    // dimensiono contenido al 85%w y 90%h de body
+    contenido.style.width = ((width * 85) / 100).toString() + 'px';
+    contenido.style.height = ((height * 90) / 100).toString() + 'px';
+    contenido.style.marginTop = '1%';
+    // posiciono el logo desde el final del contenido
+    document.getElementById('logo').style.bottom = '8%';
 };
 
-function main(){
-   var link_tao = document.querySelector('a[href*="taoteking"]'),
-       link_hua = document.querySelector('a[href*="huahuching"]'),
-       link_wen = document.querySelector('a[href*="wentzu"]'),
-       img = 0;
-   ocultarArticulos();
-   actualizar();
-   // cambio el fondo  cada 9 segundo
-   setInterval(function(){
-       if (img > 11){
+function main() {
+    var link_tao = document.querySelector('a[href*="taoteking"]')
+        , link_hua = document.querySelector('a[href*="huahuching"]')
+        , link_wen = document.querySelector('a[href*="wentzu"]')
+        , img = 0;
+    ocultarArticulos();
+    actualizar();
+    // cambio el fondo  cada 9 segundo
+    setInterval(function () {
+        if (img > 11) {
             img = 0;
-       }
-       var contenido = document.getElementById('contenedor');
-       contenido.style.backgroundImage='url(Assets/fondo' + img.toString() + '.png)';
-       img++;
-   }, 9000);
-   window.onresize = actualizar;
-   // muestro el texto del tao te king
-   link_tao.onclick = function(){
+        }
+        var contenido = document.getElementById('contenedor');
+        contenido.style.backgroundImage = 'url(Assets/fondo' + img.toString() + '.png)';
+        img++;
+    }, 9000);
+    window.onresize = actualizar;
+    // muestro el texto del tao te king
+    link_tao.onclick = function () {
         var articulo = document.getElementById('taoteking');
         menuLateral();
         seccionVisible();
@@ -98,8 +96,8 @@ function main(){
         Ajax("Textos/taoteking.txt", articulo);
         actualizar();
     };
-   // muestro el texto del hua hu ching
-   link_hua.onclick = function(){
+    // muestro el texto del hua hu ching
+    link_hua.onclick = function () {
         var articulo = document.getElementById('huahuching');
         menuLateral();
         seccionVisible();
@@ -108,8 +106,8 @@ function main(){
         Ajax("Textos/huahuching.txt", articulo);
         actualizar();
     };
-   // muestro el texto de wen tzu
-   link_wen.onclick = function(){
+    // muestro el texto de wen tzu
+    link_wen.onclick = function () {
         var articulo = document.getElementById('wentzu');
         menuLateral();
         seccionVisible();
